72118001f135dc6eb79655f7e87aba105def2326
Arvind Suresh <arvind.suresh@gmail.com>
2011-12-18 09:54:11 -0800

    Removing comments from settings.py

--- settings.py
+++ settings.py
@@ -117,8 +117,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
